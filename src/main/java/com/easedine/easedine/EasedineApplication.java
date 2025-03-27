cab2646fc22f7632eaf912cf57a97683f33d4220
Arnesh Rajendran <arnesha4261@gmail.com>
2025-03-28 00:13:21 +0530

    First commit

---
+++ src/main/java/com/easedine/easedine/EasedineApplication.java
@@ -0,0 +1,13 @@
+package com.easedine.easedine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasedineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasedineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easedine/easedine/EasedineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easedine.easedine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasedineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
